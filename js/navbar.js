c804f083ed55b690583ca51f27583c73a0a8269d
martin <martin.storodegard@gmail.com>
2017-09-13 14:43:20 +0200

    Martin webpage project added to dev

---
+++ js/navbar.js
@@ -0,0 +1,18 @@
+function navbar_open() {
+    document.getElementById("myNavbar").style.display = "block";
+}
+
+function navbar_close() {
+    document.getElementById("myNavbar").style.display = "none";
+}
+
+var displayDescriptionField = true;
+function toggleDescriptionField(){
+	if (displayDescriptionField){
+		displayDescriptionField = false;
+		document.getElementById("showDescription").id = "hideDescription";
+	}else{
+		displayDescriptionField = true;
+		document.getElementById("hideDescription").id = "showDescription";
+	}
+}
